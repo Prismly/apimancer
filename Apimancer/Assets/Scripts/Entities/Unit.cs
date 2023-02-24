b31aa6d1b7f571d791aace85b0199b1855332686
waowens2 <waowens2@ncsu.edu>
2023-02-24 18:27:55 -0500

    Set up MVP units
    TODO:
    
    Actions
    Target selection
    Death

--- Apimancer/Assets/Scripts/Entities/Unit.cs
+++ Apimancer/Assets/Scripts/Entities/Unit.cs
@@ -2,7 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Unit : Entity
+public abstract class Unit : Entity
 {
-    
+    public enum Faction
+    {
+        RESOURCE,
+        BEE,
+        ANT
+    }
+
+    public abstract Faction UnitFaction { get; set; }
+    public abstract float MaxHealth { get; set; }
+    public abstract float Health { get; set; }
+    public abstract float AttackDamage { get; set; }
+    public abstract float MovementSpeed { get; set; }
+
+    // static deal damage to target
+    public static void DamageTarget(float dmg, Unit target) {
+        target.ReceiveDamage(dmg);
+    }
+
+    // receive damage
+    protected virtual void ReceiveDamage(float dmg) 
+    {
+        this.Health -= dmg;
+    }
 }
