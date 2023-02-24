cbebd6588377cb1031598c97282e2c2fd66a9d2c
Isaiah Che <hunter66888@gmail.com>
2023-02-24 13:40:29 -0500

    Account for distance in movement cost

--- Apimancer/Assets/Scripts/Entity.cs
+++ Apimancer/Assets/Scripts/Entity.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 public class Entity : MonoBehaviour
 {
-    [SerializeField] private Cell cell;
+    [SerializeField] public Cell cell { get; set; }
 
     public static List<Cell> PathFind(Entity entity, Cell end)
     {
@@ -13,7 +13,7 @@ public class Entity : MonoBehaviour
         List<Cell> closed = new List<Cell>();
         Cell current = entity.cell;
         current.G = 0;
-        current.H = entity.MovementCost(end);
+        current.H = entity.MovementCost(entity.cell, end);
 
         open.Add(current);
         while (open.Count != 0)
@@ -44,7 +44,7 @@ public class Entity : MonoBehaviour
                 if (!(open.Contains(adjacent)))
                 {
                     adjacent.G = g;
-                    adjacent.H = entity.MovementCost(end);
+                    adjacent.H = entity.MovementCost(adjacent, end);
                     open.Add(adjacent);
                 }
                 // Otherwise check if using current G we can get a lower value of F, if so update it's value.
@@ -75,7 +75,7 @@ public class Entity : MonoBehaviour
         return finalPathTiles;
     }
 
-    private int MovementCost(Cell end)
+    protected virtual int MovementCost(Cell c, Cell end)
     {
         return 0;
     }
