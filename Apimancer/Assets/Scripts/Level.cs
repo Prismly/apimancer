3ae2f54ed695fa2bda44c40f92b3a422fc7a8e07
FlizFlaz <jacksonpeck523@gmail.com>
2023-03-02 13:20:23 -0500

    Add level script and support in game manager

---
+++ Apimancer/Assets/Scripts/Level.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] private List<Vector2Int> _spawnLocations;
+
+    public void SpawnWizards(List<Wizard> wizards)
+    {
+        int wizardCount = Mathf.Min(wizards.Count, _spawnLocations.Count);
+        for (int i = 0; i < wizardCount; i++)
+        {
+            // Set wizard cell position here
+        }
+    }
+
+    public int getMaxWizards()
+    {
+        return _spawnLocations.Count;
+    }
+}
